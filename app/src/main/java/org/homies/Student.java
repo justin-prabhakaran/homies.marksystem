db0e831d82da9d9830bfaa29bc60db9574f06ef1
justinprabhakaran <c.m.justinprabhakaran@gmail.com>
2024-04-08 15:33:59 +0530

    added abstraction

---
+++ app/src/main/java/org/homies/Student.java
@@ -0,0 +1,21 @@
+package org.homies;
+
+public class Student {
+    private String name;
+    private Mark marks;
+
+    public Mark getMarks() {
+        return marks;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setMarks(Mark marks) {
+        this.marks = marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
